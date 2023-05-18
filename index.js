a36dc0ce4042ebbe3956b3e3528f9ca81270aed1
divyu27 <130134834+divyu27@users.noreply.github.com>
2023-05-18 23:03:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express()
+const port=3000
+const path=require('path')
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'hey.html'))
+})
+app.listen(port,()=>{
+    console.log(`app listening at ${port}`)
+})
